56a79dc93711acd776f3edeb899bfc35396217c3
Andy Frievalt <afrievalt@yahoo.com>
2023-06-05 09:57:59 -0500

    fix plop

--- plopfile.js
+++ plopfile.js
@@ -1,8 +1,6 @@
 const _plop = require('./plop-templates/_plop')
-const _table = require('./plop-templates/_table')
 
 module.exports = function (plop) {
   // controller generator
-plop.setGenerator('table', _table)
   plop.setGenerator('plop', _plop)
 }
